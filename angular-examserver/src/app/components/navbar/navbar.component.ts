9cb16fe9303852839c0954c33b273b4616f035ce
Lalookumar19227 <imykl27@gmail.com>
2025-06-23 19:52:22 +0530

    nav bar use for different use cases

--- angular-examserver/src/app/components/navbar/navbar.component.ts
+++ angular-examserver/src/app/components/navbar/navbar.component.ts
@@ -1,35 +1,51 @@
-import { Component } from '@angular/core';
+import { Component,Input} from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
+import { OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
 
-isLoggedIn = false;
-user = null;
+@Input() userRole1!: string;
 
-  constructor(public login:LoginService) {}
+  isLoggedIn = false;
+  user = null;
+userRole: any;
+// currentRoute: string = '';
 
-  ngOnInit(): void{
-this.isLoggedIn = this.login.isLoggedIn();
-this.user = this.login.getUser();
-this.login.loginStatusSubject.asObservable().subscribe((data)=>{
-  this.isLoggedIn=this.login.isLoggedIn();
-  this.user=this.login.getUser();
-});
+  constructor(public login: LoginService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.login.isLoggedIn();
+    this.user = this.login.getUser();
+    // this.userRole= "Admin"
+    this.login.loginStatusSubject.asObservable().subscribe((data) => {
+      this.isLoggedIn = this.login.isLoggedIn();
+      this.user = this.login.getUser();
+    });
+    //  this.currentRoute = this.router.url;
+
+  // ⬇️ NEW: Subscribe to route changes
+  // this.router.events.subscribe(event => {
+  //   if (event instanceof NavigationEnd) {
+  //     this.currentRoute = event.urlAfterRedirects;
+  //   }
+  // });
+
   }
 
-public logout() {
-  this.login.logout();
-  // this.isLoggedIn = false;
-  // this.user = null;
-  window.location.reload();
-  // this.login.loginStatusSubject.next(false);
-
+  public logout() { 
+    this.login.logout();
+    // this.isLoggedIn = false;
+    // this.user = null;
+    window.location.reload();
+    // this.login.loginStatusSubject.next(false);
+  }
 }
 
 
-}
